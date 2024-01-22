925a202034947ee94681b236d211ef4e58cbea17
Gioele Masini <gioele.masini@dynaset.sm>
2024-01-23 00:19:26 +0100

    Move test code in renderer-worker.ts

--- src/renderer/renderer-worker.ts
+++ src/renderer/renderer-worker.ts
@@ -22,7 +22,53 @@ function redrawOffscreenCanvas(time: string) {
 	ctxWorker.textAlign = 'center';
 	ctxWorker.fillStyle = 'lightblue';
 	ctxWorker.fillText(time, canvasWorker.width / 2, canvasWorker.height / 2);
+	// The code below is only for testing purposes
+	imagesToDraw.forEach(
+		({image, dx, dy}) => {
+			ctxWorker.drawImage(image, dx, dy);
+		},
+	);
 }
+
+// The code below is only for testing purposes
+const imagesToDraw: ImageToDraw[] = [];
+
+function loadTestImage() {
+	const img = new Image();
+	img.src = 'https://photonstorm.com/wp-content/uploads/2011/09/favicons-smallFFS.gif';
+	img.onload = () => {
+		void Promise.all([
+			createImageBitmap(img),
+			createImageBitmap(img, 16, 0, 16, 16),
+			createImageBitmap(img, 48, 0, 16, 16),
+			createImageBitmap(img, 80, 0, 16, 16),
+			createImageBitmap(img, 0, 16, 16, 16),
+			createImageBitmap(img, 32, 16, 16, 16),
+			createImageBitmap(img, 64, 16, 16, 16),
+			createImageBitmap(img, 16, 32, 16, 16),
+			createImageBitmap(img, 48, 32, 16, 16),
+			createImageBitmap(img, 80, 32, 16, 16),
+		]).then(
+			([spritesheet, ...sprites]) => {
+				imagesToDraw.push({image: spritesheet, dx: 0, dy: 0});
+
+				sprites.forEach(
+					(sprite, index) => {
+						imagesToDraw.push({image: sprite, dx: index * 16, dy: 100});
+					},
+				);
+			},
+		);
+	};
+}
+
+loadTestImage();
+
+type ImageToDraw = {
+	image: ImageBitmap;
+	dx: number;
+	dy: number;
+};
 
 export type RenderWorkerConfig = {
 	canvas: OffscreenCanvas;
