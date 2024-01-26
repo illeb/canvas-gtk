502ef6bb0b4260a5d955167a8a77d780fcf932bb
stefano belli <stefano.belli@weroad.it>
2024-01-26 09:27:03 +0100

    some test with web worker

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,8 +1,11 @@
+import ViteWorker from './renderer-worker.ts?worker';
+
 export function setupRender(canvas: HTMLCanvasElement) {
 	const offscreen = canvas.transferControlToOffscreen();
-	const worker = new Worker(new URL('./renderer-worker.ts', import.meta.url), {
-		type: 'module',
-	});
-	worker.postMessage({canvas: offscreen}, [offscreen]);
-	return worker;
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
+	const tsWorker = new ViteWorker();
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+	tsWorker.postMessage({canvas: offscreen}, [offscreen]);
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	return tsWorker;
 }
