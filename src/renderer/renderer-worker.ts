aebc456ede5545b05e5e69948a7e762e8dec7cfe
stefano belli <stefano.belli@weroad.it>
2024-01-27 08:50:47 +0100

    fixed web worker problems, now it's inline and works

--- src/renderer/renderer-worker.ts
+++ src/renderer/renderer-worker.ts
@@ -1,5 +1,79 @@
-self.onmessage = (event: MessageEvent<number>) => {
-	self.postMessage(event.data + 1);
+let canvasWorker: OffscreenCanvas;
+let ctxWorker: OffscreenCanvasRenderingContext2D;
+
+self.onmessage = (evt: MessageEvent<RenderWorkerConfig>) => {
+	const {canvas} = evt.data;
+	canvasWorker = canvas;
+	ctxWorker = canvas.getContext('2d')!;
+
+	function render(time: DOMHighResTimeStamp) {
+		// Perform some drawing using the gl context
+		redrawOffscreenCanvas(time.toPrecision());
+
+		requestAnimationFrame(render);
+	}
+
+	requestAnimationFrame(render);
+	self.onmessage = (event: MessageEvent<number>) => {
+		self.postMessage(event.data + 1);
+	};
 };
 
-export {};
+function redrawOffscreenCanvas(time: string) {
+	ctxWorker.clearRect(0, 0, canvasWorker.width, canvasWorker.height);
+	ctxWorker.font = '24px Verdana';
+	ctxWorker.textAlign = 'center';
+	ctxWorker.fillStyle = 'lightblue';
+	ctxWorker.fillText(time, canvasWorker.width / 2, canvasWorker.height / 2);
+	// The code below is only for testing purposes
+	imagesToDraw.forEach(
+		({image, dx, dy}) => {
+			ctxWorker.drawImage(image, dx, dy);
+		},
+	);
+}
+
+// The code below is only for testing purposes
+const imagesToDraw: ImageToDraw[] = [];
+
+function loadTestImage() {
+	const img = new Image();
+	img.src = 'https://photonstorm.com/wp-content/uploads/2011/09/favicons-smallFFS.gif';
+	img.onload = () => {
+		void Promise.all([
+			createImageBitmap(img),
+			createImageBitmap(img, 16, 0, 16, 16),
+			createImageBitmap(img, 48, 0, 16, 16),
+			createImageBitmap(img, 80, 0, 16, 16),
+			createImageBitmap(img, 0, 16, 16, 16),
+			createImageBitmap(img, 32, 16, 16, 16),
+			createImageBitmap(img, 64, 16, 16, 16),
+			createImageBitmap(img, 16, 32, 16, 16),
+			createImageBitmap(img, 48, 32, 16, 16),
+			createImageBitmap(img, 80, 32, 16, 16),
+		]).then(
+			([spritesheet, ...sprites]) => {
+				imagesToDraw.push({image: spritesheet, dx: 0, dy: 0});
+
+				sprites.forEach(
+					(sprite, index) => {
+						imagesToDraw.push({image: sprite, dx: index * 16, dy: 100});
+					},
+				);
+			},
+		);
+	};
+}
+
+loadTestImage();
+
+type ImageToDraw = {
+	image: ImageBitmap;
+	dx: number;
+	dy: number;
+};
+
+export type RenderWorkerConfig = {
+	canvas: OffscreenCanvas;
+};
+
--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,11 +1,8 @@
-import ViteWorker from './renderer-worker.ts?worker';
+import ViteWorker from './renderer-worker.ts?worker&inline';
 
 export function setupRender(canvas: HTMLCanvasElement) {
 	const offscreen = canvas.transferControlToOffscreen();
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
 	const tsWorker = new ViteWorker();
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 	tsWorker.postMessage({canvas: offscreen}, [offscreen]);
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 	return tsWorker;
 }
